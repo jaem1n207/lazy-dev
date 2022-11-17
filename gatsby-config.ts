59e88efad9ba53453d3dff5692194f52781409b4
jaem1n207 <roy.jm.lee@gmail.com>
2022-11-18 01:50:14 +0900

    chore: manifest 관련 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,11 +2,16 @@ import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `lazyDev`,
+    title: `Lazy Dev`,
+    description: `jaemin's front end dev blog`,
     siteUrl: `https://lazyDev.gatsbyjs.io`,
+    author: `Jaemin Lee`,
+    lang: `ko`,
+    links: {
+      github: `https://github.com/jaem1n207/lazy-dev`,
+    },
+    favicon: `./src/images/favicon.png`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
@@ -15,7 +20,57 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
+        name: 'Lazy Dev',
+        short_name: 'Lazy Dev',
+        description: 'jaemin`s front end dev blog',
+        start_url: '/',
+        background_color: '#282c35',
+        theme_color: '#efaec1',
+        display: 'standalone',
         icon: 'src/images/icon.png',
+        icons: [
+          {
+            src: './public/icons/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        crossOrigin: `use-credentials`,
       },
     },
     'gatsby-plugin-mdx',
