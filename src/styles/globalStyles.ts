8a2d65822cb12df284a55a26d094211b6a5f8c17
jaem1n207 <roy.jm.lee@gmail.com>
2022-11-27 22:44:18 +0900

    feat: font preload

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -3,7 +3,6 @@ import tw, { theme } from 'twin.macro';
 
 const customStyles = css({
   'html, body': {
-    fontFamily: 'Noto Sans KR, sans-serif',
     backgroundColor: theme`colors.gray.100`,
     ...tw`antialiased`,
   },
