a0ae56d89e2c7e730fb148da0c35abd908afe575
jaem1n207 <roy.jm.lee@gmail.com>
2022-12-02 02:45:24 +0900

    chore: set cache-control header

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -38,6 +38,22 @@ const corePlugins: GatsbyConfig['plugins'] = [
       path: `${__dirname}/src/images`,
     },
   },
+  {
+    resolve: `gatsby-plugin-gatsby-cloud`,
+    options: {
+      headers: {
+        '/static/fonts/*': ['Cache-Control: public, max-age=31536000, immutable'],
+        '/(.*\\.(js|json|css|ico|png)$)': ['Cache-Control: public, max-age=31536000, immutable'],
+        '/(.*)': ['Cache-Control: public, max-age=0, must-revalidate'],
+      }, // option to add more headers. `Link` headers are transformed by the below criteria
+      allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+      mergeSecurityHeaders: true, // boolean to turn off the default security headers
+      mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+      mergeCachingHeaders: true, // boolean to turn off the default caching headers
+      transformHeaders: (headers: any, path: any) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+      generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+    },
+  },
 ];
 
 const devPlugins: GatsbyConfig['plugins'] = [
