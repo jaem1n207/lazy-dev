183f248d02576343b344d6a2358197a642bcbefe
jaem1n207 <roy.jm.lee@gmail.com>
2022-12-05 18:14:43 +0900

    refactor: gatsby node

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,19 @@ import * as React from 'react';
 import { graphql, HeadFC, HeadProps, PageProps } from 'gatsby';
 import tw from 'twin.macro';
 
-import Contents from 'Components/contents';
+// import Contents from 'Components/contents';
 import Seo from 'Components/seo';
 import Layout from 'Layout/layout';
 
 const IndexPage: React.FC<PageProps<Queries.HomepageQuery>> = ({ data, location }) => {
   const title = data.site?.siteMetadata?.title || null;
 
-  const posts = data.allMarkdownRemark;
+  // const posts = data.allMarkdownRemark;
 
   return (
     <Layout location={location} title={title}>
       <h1 css={tw`bg-amber-200 `}>Hello</h1>
-      <Contents posts={posts} />
+      {/* <Contents posts={posts} /> */}
     </Layout>
   );
 };
@@ -33,20 +33,20 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            category
-          }
-        }
-      }
-    }
+    # allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+    #   edges {
+    #     node {
+    #       excerpt
+    #       fields {
+    #         slug
+    #       }
+    #       frontmatter {
+    #         date(formatString: "MMMM DD, YYYY")
+    #         title
+    #         # category
+    #       }
+    #     }
+    #   }
+    # }
   }
 `;
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,78 +1,85 @@
-import React from 'react';
+// import * as React from 'react';
 
-import { graphql, PageProps, HeadFC, HeadProps } from 'gatsby';
+// import { graphql, PageProps, HeadProps } from 'gatsby';
 
-import Seo from 'Components/seo';
-import Layout from 'Layout/layout';
+// import Seo from 'Components/seo';
+// import Layout from 'Layout/layout';
 
-interface MarkdownRemark {
-  html: string;
-  excerpt: string;
-  fields: {
-    slug: string;
-  };
-  frontmatter: {
-    date: string;
-    title: string;
-    category: string;
-  };
-}
+// type DataProps = {
+//   markdownRemark: {
+//     html: string;
+//     excerpt: string;
+//     fields: {
+//       slug: string;
+//     };
+//     frontmatter: {
+//       date: string;
+//       title: string;
+//       // category: string;
+//       description: string;
+//     };
+//   };
+// };
 
-const blogPost: React.FC<PageProps<Queries.BlogPostBySlugQuery>> = ({ data, location }) => {
-  const title = data.site?.siteMetadata?.title || `Title`;
+// const blogPost: React.FC<PageProps<Queries.BlogPostBySlugQuery>> = ({ data, location }) => {
+//   const title = data.site?.siteMetadata?.title || `Title`;
 
-  return (
-    <Layout location={location} title={title}>
-      blogPost
-    </Layout>
-  );
-};
+//   return (
+//     <Layout location={location} title={title}>
+//       blogPost
+//     </Layout>
+//   );
+// };
 
-export const Head: HeadFC = ({ data: { markdownRemark: post } }: HeadProps) => {
-  return (
-    <Seo
-      title={post.frontmatter.title}
-      description={post.frontmatter.description || post.excerpt}
-    />
-  );
-};
+// export const Head = ({ data: { markdownRemark: post } }: HeadProps<DataProps>) => {
+//   return (
+//     <Seo
+//       title={post.frontmatter.title}
+//       description={post.frontmatter.description || post.excerpt}
+//     />
+//   );
+// };
 
-export default blogPost;
+// export default blogPost;
 
-export const pageQuery = graphql`
-  query BlogPostBySlug($id: String!, $previousPostId: String, $nextPostId: String) {
-    site {
-      siteMetadata {
-        title
-        author
-        siteUrl
-      }
-    }
-    markdownRemark(id: { eq: $id }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-    previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-    next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query BlogPostBySlug($id: String!, $previousPostId: String, $nextPostId: String) {
+//     site {
+//       siteMetadata {
+//         title
+//         author {
+//           name
+//           summary
+//         }
+//         siteUrl
+//       }
+//     }
+//     markdownRemark(id: { eq: $id }) {
+//       id
+//       excerpt(pruneLength: 160)
+//       html
+//       frontmatter {
+//         title
+//         date(formatString: "MMMM DD, YYYY")
+//         # category
+//         description
+//       }
+//     }
+//     previous: markdownRemark(id: { eq: $previousPostId }) {
+//       fields {
+//         slug
+//       }
+//       frontmatter {
+//         title
+//       }
+//     }
+//     next: markdownRemark(id: { eq: $nextPostId }) {
+//       fields {
+//         slug
+//       }
+//       frontmatter {
+//         title
+//       }
+//     }
+//   }
+// `;
