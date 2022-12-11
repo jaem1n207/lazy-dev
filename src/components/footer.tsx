12e5c87b3ed797d22f9babdbb4843252249f4111
jaem1n207 <roy.jm.lee@gmail.com>
2022-12-11 23:35:44 +0900

    feat: rss 푸터에 추가

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 
+import tw from 'twin.macro';
+
+import { UnderlineLink } from 'Components/underlineLink';
+
 const Footer = () => {
   return (
-    <footer>
+    <footer css={tw`mt-70pxr pt-28pxr`}>
       @ {new Date().getFullYear()}, Built with
       {` `}
-      <a href="https://github.com/jaem1n207/lazy-dev">Lazy Dev</a>
+      <UnderlineLink href="https://github.com/jaem1n207/lazy-dev">Lazy Dev</UnderlineLink>
+      <div css={tw`float-right`}>
+        <UnderlineLink href="/rss.xml" target="_blank" rel="noopener noreferrer">
+          rss
+        </UnderlineLink>
+      </div>
     </footer>
   );
 };
