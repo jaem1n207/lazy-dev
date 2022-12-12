114cbed528399d614c0bba3978c21e55d7e27ef4
jaem1n207 <roy.jm.lee@gmail.com>
2022-12-13 02:20:56 +0900

    feat: dark theme

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,10 +2,18 @@ import { css } from '@emotion/react';
 import tw, { theme } from 'twin.macro';
 
 const customStyles = css({
-  'html, body': {
+  body: {
+    backgroundColor: theme`colors.white`,
+    color: theme`colors.dark`,
+    minHeight: '100vh',
+    transitionProperty: 'background-color, color',
+    transitionDuration: '0.2s',
+    transitionTimingFunction: 'ease-in-out',
+    ...tw`antialiased`,
+  },
+  'html.dark body': {
     backgroundColor: theme`colors.dark`,
     color: theme`colors.white`,
-    ...tw`antialiased`,
   },
   header: {
     fontFamily: 'Spoqa Han Sans Neo',
