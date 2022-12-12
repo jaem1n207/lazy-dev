f9f350dc66c0fdb5076899d65ff718b30b095961
jaem1n207 <roy.jm.lee@gmail.com>
2022-12-12 18:20:10 +0900

    chore: theme color ssr

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -27,5 +27,32 @@ export const onRenderBody: GatsbySSR['onRenderBody'] = ({
       type="font/woff2"
       crossOrigin="anonymous"
     />,
+    <script
+      key="darkmode"
+      dangerouslySetInnerHTML={{
+        __html: `(function() {  
+            function setTheme(theme) {
+              window.__theme = theme;
+              if (theme === 'dark') {
+                document.documentElement.className = 'dark';
+              } else {
+                document.documentElement.className = '';
+              }
+            };
+            window.__setPreferredTheme = function(theme) {
+              setTheme(theme);
+              try {
+                localStorage.setItem('color-theme', theme);
+              } catch (e) {}
+            };
+            let preferredTheme;
+            try {
+              preferredTheme = localStorage.getItem('color-theme');
+            } catch (e) {}
+            let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+            setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+          })();`,
+      }}
+    />,
   ]);
 };
