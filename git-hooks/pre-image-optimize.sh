7e97e5adc31f7f888c0c482fa99d47c1689dfc35
jaem1n207 <roy.jm.lee@gmail.com>
2022-12-29 02:41:10 +0900

    chore: image-optimization git hook 폴더 삭제 및 조건문 스크립트 수정

--- git-hooks/pre-image-optimize.sh
+++ git-hooks/pre-image-optimize.sh
@@ -3,6 +3,6 @@ mkdir ~/.image-opt
 curl --output ~/.image-opt/imageoptim-cli.zip https://codeload.github.com/JamieMason/ImageOptim-CLI/zip/1.15.1
 unzip ~/.image-opt/imageoptim-cli.zip -d ~/.image-opt/tmp
 mv ~/.image-opt/tmp/ImageOptim-CLI-1.15.1 ~/.image-opt/imageoptim-cli
-rm -rf ~/.image-opt/tmp
+rm -r ~/.image-opt
 
 export PATH=$PATH:~/.image-opt/imageoptim-cli/bin
