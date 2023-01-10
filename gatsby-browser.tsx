1b3cae87fe29c0a55fa4c1ecefddb976b7f4c70c
jaem1n207 <roy.jm.lee@gmail.com>
2023-01-11 02:33:26 +0900

    🧹 chore: location.action이 push가 아니라면 페이지 스크롤 복구

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,3 +2,13 @@ import '@fontsource/fira-mono';
 
 import './src/styles/global.css';
 import 'prismjs/themes/prism-tomorrow.css';
+
+export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0), 0);
+  } else {
+    const savedPosition = getSavedScrollPosition(location);
+    window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), 0);
+  }
+  return false;
+};
