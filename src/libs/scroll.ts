9a65ede6d74f2e3e908e0ab042dcd3e4ff9114b9
jaem1n207 <roy.jm.lee@gmail.com>
2023-01-11 13:01:51 +0900

    ✨ feat: smooth scroll polyfill 및 블로그 포스트에 적용

--- src/libs/scroll.ts
+++ src/libs/scroll.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * @fileoverview Scroll to anchor link with smooth scroll polyfill and smooth-scroll library
  * @see https://github.com/JaeYeopHan/gatsby-starter-bee/blob/master/src/utils/scroll.js
@@ -10,6 +11,11 @@ import smoothscroll from 'smoothscroll-polyfill';
 
 import { isBrowser } from './environment';
 
+=======
+import SmoothScroll from 'smooth-scroll';
+import smoothscroll from 'smoothscroll-polyfill';
+
+>>>>>>> 0a4c3e7 (✨ feat: smooth scroll polyfill 및 블로그 포스트에 적용)
 let scroll: SmoothScroll | null = null;
 
 export function init() {
@@ -31,7 +37,10 @@ export function destroy() {
 }
 
 export function go(dest: number) {
+<<<<<<< HEAD
   if (!isBrowser) return null;
+=======
+>>>>>>> 0a4c3e7 (✨ feat: smooth scroll polyfill 및 블로그 포스트에 적용)
   if (!scroll) throw Error('Not founded SmoothScroll instance');
 
   if (dest < window.scrollY) {
