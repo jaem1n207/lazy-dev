03aacf230bc7010fff483a4493d380f4e5055280
jaem1n207 <roy.jm.lee@gmail.com>
2023-01-11 13:02:24 +0900

    ✨ feat: useWindowLocation hook

--- src/hooks/use-window-location.tsx
+++ src/hooks/use-window-location.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+<<<<<<< HEAD
 import { isBrowser } from 'Libs/environment';
 
+=======
+>>>>>>> 8a7b240 (✨ feat: useWindowLocation hook)
 import { useIsMounted } from './use-is-mounted';
 
 export const useWindowLocation = () => {
   const isMounted = useIsMounted();
+<<<<<<< HEAD
   const [location, setLocation] = React.useState(
     isMounted() && isBrowser ? window.location : undefined
   );
@@ -13,6 +17,11 @@ export const useWindowLocation = () => {
   React.useEffect(() => {
     if (!isBrowser) return;
 
+=======
+  const [location, setLocation] = React.useState(isMounted() ? window.location : undefined);
+
+  React.useEffect(() => {
+>>>>>>> 8a7b240 (✨ feat: useWindowLocation hook)
     const handleLocationChange = () => {
       setLocation(window.location);
     };
