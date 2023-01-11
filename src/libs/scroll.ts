0a4c3e7648fb2de3036d2f13ee0f904ecf37c137
jaem1n207 <roy.jm.lee@gmail.com>
2023-01-11 13:01:51 +0900

    ✨ feat: smooth scroll polyfill 및 블로그 포스트에 적용

---
+++ src/libs/scroll.ts
@@ -0,0 +1,32 @@
+import SmoothScroll from 'smooth-scroll';
+import smoothscroll from 'smoothscroll-polyfill';
+
+let scroll: SmoothScroll | null = null;
+
+export function init() {
+  smoothscroll.polyfill();
+  scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true,
+  });
+  return scroll;
+}
+
+export function destroy() {
+  if (!scroll) throw Error('Not founded SmoothScroll instance');
+
+  scroll.destroy();
+  scroll = null;
+
+  return scroll;
+}
+
+export function go(dest: number) {
+  if (!scroll) throw Error('Not founded SmoothScroll instance');
+
+  if (dest < window.scrollY) {
+    scroll.animateScroll(dest);
+  }
+
+  return scroll;
+}
