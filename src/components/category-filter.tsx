58692171d181f31a1bdb67fd361ee2040233839a
jaem1n207 <roy.jm.lee@gmail.com>
2023-01-18 02:24:03 +0900

    🐛 fix: category-filter button

--- src/components/category-filter.tsx
+++ src/components/category-filter.tsx
@@ -1,8 +1,8 @@
-import React, { memo, useMemo } from 'react';
+import React, { memo, useEffect, useMemo } from 'react';
 
-import { GatsbyLinkProps } from 'gatsby';
 import tw from 'twin.macro';
 
+import { useCategory } from 'Hooks/use-category';
 import useScrollCenter from 'Hooks/use-scroll-center';
 import { firstLetterUppercase, kebabCase } from 'Libs/string';
 import { CATEGORY_TYPE } from 'Types/enum';
@@ -13,8 +13,6 @@ interface CategoryFilterProps {
   resetCategory: () => void;
 }
 
-type LinkPropsGetter = GatsbyLinkProps<unknown>['getProps'];
-
 const ACTIVE_ID = 'active';
 
 const Nav = tw.nav`z-20 sticky top-0pxr flex items-center rounded-lg bg-secondary mb-48pxr py-12pxr px-24pxr [button]:(block font-bold transition-colors rounded-lg py-8pxr px-16pxr text-14pxr hover:(text-background bg-primary)) [button[data-ui=${ACTIVE_ID}]]:(text-background bg-primary)`;
@@ -22,41 +20,54 @@ const Nav = tw.nav`z-20 sticky top-0pxr flex items-center rounded-lg bg-secondar
 const CategoryFilter = ({ categories, selectCategory, resetCategory }: CategoryFilterProps) => {
   const categoryListRef = React.useRef<HTMLUListElement>(null);
 
-  const linkProps: LinkPropsGetter = ({ location, href }) => {
-    const category = new URLSearchParams(location.search).get('category');
-    const activeCategory = href.split('?category=')[1];
-    const isCategoryActive = category === activeCategory;
-    const isAll = !category && !activeCategory;
+  const { category } = useCategory();
 
-    return (isCategoryActive || isAll ? ACTIVE_ID : undefined)
-      ? {
-          'data-ui': ACTIVE_ID,
-          tabIndex: -1,
-        }
-      : {};
-  };
-
-  useScrollCenter({ listRef: categoryListRef, targetId: ACTIVE_ID });
+  const isAll = category === CATEGORY_TYPE.ALL;
 
   const sortedCategories = useMemo(
     () => [...categories].sort((a, b) => b.totalCount - a.totalCount),
     [categories]
   );
 
+  useScrollCenter({ listRef: categoryListRef, targetId: ACTIVE_ID });
+
+  useEffect(() => {
+    const activeCategory = sortedCategories.find((categoryInfo) => {
+      const { fieldValue } = categoryInfo;
+
+      return fieldValue === category;
+    });
+
+    if (activeCategory?.fieldValue === category) {
+      console.log('scroll center');
+      // selectCategory(category);
+    }
+  }, [category, selectCategory, sortedCategories]);
+
   return (
     <Nav>
-      <button onClick={resetCategory}>{CATEGORY_TYPE.ALL}</button>
+      <button
+        onClick={resetCategory}
+        data-ui={isAll ? ACTIVE_ID : undefined}
+        tabIndex={isAll ? -1 : undefined}
+      >
+        {CATEGORY_TYPE.ALL}
+      </button>
       <div className="w-1pxr h-32pxr mx-8pxr -translate-x-[50%] bg-divider" />
       <ul
         ref={categoryListRef}
         className="flex overflow-x-auto scroll-smooth gap-8pxr scrollbar-hide"
       >
-        {sortedCategories.map((category) => {
-          const { fieldValue } = category;
+        {sortedCategories.map((categoryInfo) => {
+          const { fieldValue } = categoryInfo;
 
           return (
             <li key={fieldValue} className="mb-0pxr">
-              <button onClick={selectCategory.bind(null, kebabCase(fieldValue!))}>
+              <button
+                onClick={selectCategory.bind(null, kebabCase(fieldValue!))}
+                data-ui={category === fieldValue ? ACTIVE_ID : undefined}
+                tabIndex={category === fieldValue ? -1 : undefined}
+              >
                 {firstLetterUppercase(fieldValue!)}
               </button>
             </li>
