6b168f23f639b55fee59eb1e9687c88d5e93d604
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-07 02:38:59 +0900

    🐛 fix: scroll issue

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,24 +5,14 @@ import 'prismjs/themes/prism-tomorrow.css';
 
 const UPDATE_SCROLL_TIME_OUT = 1;
 
-// TODO: navigate 확인, 스크롤 초기화되는 문제 확인, Link Component로 동작 확인(ref type error)
-
 export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
-  if (location.action === 'PUSH') {
+  const isRootPath = location.pathname === '/';
+
+  if (!isRootPath && location.action === 'PUSH') {
     window.setTimeout(() => window.scrollTo(0, 0), 0);
-  } else {
+  } else if (isRootPath && location.action == null) {
     const savedPosition = getSavedScrollPosition(location);
     window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), UPDATE_SCROLL_TIME_OUT);
   }
   return false;
 };
-
-// export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
-//   if (location.action === 'PUSH') {
-//     window.setTimeout(() => window.scrollTo(0, 0), 0);
-//   } else {
-//     const savedPosition = getSavedScrollPosition(location);
-//     window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), UPDATE_SCROLL_TIME_OUT);
-//   }
-//   return false;
-// };
--- src/hooks/use-category.ts
+++ src/hooks/use-category.ts
@@ -1,18 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 
 import { navigate } from 'gatsby';
+import queryString from 'query-string';
 
 import { isBrowser } from 'Libs/environment';
 import * as ScrollManager from 'Libs/scroll';
 import { CATEGORY_TYPE } from 'Types/enum';
 
-import { useQueryParams } from './use-query-params';
-
 const DEST_TOP = 114;
 
 export const useCategory = () => {
   const [category, setCategory] = useState(CATEGORY_TYPE.ALL);
-  const categoryQueryParams = useQueryParams({ key: 'category', type: 'string' });
 
   const selectCategory = useCallback((category: string) => {
     if (!isBrowser) return null;
@@ -29,19 +27,17 @@ export const useCategory = () => {
     navigate('/');
   }, []);
 
-  const changeCategory = useCallback(
-    (withScroll = true) => {
-      const target = !categoryQueryParams ? CATEGORY_TYPE.ALL : categoryQueryParams;
+  const changeCategory = useCallback((withScroll = true) => {
+    const { category } = queryString.parse(location.search);
+    const target = category == null ? CATEGORY_TYPE.ALL : category;
+    if (target == null || Array.isArray(target)) return;
 
-      setCategory(target);
-      if (withScroll) {
-        ScrollManager.go(DEST_TOP);
-      } else {
-        ScrollManager.go(0);
-      }
-    },
-    [categoryQueryParams]
-  );
+    setCategory(target);
+
+    if (withScroll) {
+      ScrollManager.go(DEST_TOP);
+    }
+  }, []);
 
   useEffect(() => {
     ScrollManager.init();
