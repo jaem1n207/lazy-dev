926f3b94c9678589b21a92b30f46ab8bd1b2c3b3
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-10 18:36:40 +0900

    ♻️ refactor: portals context type hooks 제거

--- src/components/portals/libs/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-import { PortalsContainerElement } from './types';
-
-export interface PortalsManager {
-  container: PortalsContainerElement;
-}
-
-export const PortalsManagerContext = createContext<PortalsManager | null>(null);
--- src/components/portals/libs/types.ts
+++
@@ -1,1 +0,0 @@
-export type PortalsContainerElement = HTMLDivElement | null;
