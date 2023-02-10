f11aaab1aabf4ff57f6de2f8e228bc05d46db10d
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-10 18:50:03 +0900

    ✨ feat: uuid util 함수 작성

--- src/libs/string.ts
+++ src/libs/string.ts
@@ -5,3 +5,13 @@ export const kebabCase = (str: string) =>
     .toLowerCase();
 
 export const firstLetterUppercase = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const uuid = () => {
+  let dt = new Date().getTime();
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
+};
