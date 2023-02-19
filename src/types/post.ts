553b2813107c54708e0b2e69b69e6692fe8ca7f5
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-20 03:25:54 +0900

    ✨ feat: 검색 기능 개발

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,5 +1,8 @@
 export default interface Post
-  extends Pick<Queries.MarkdownRemarkFrontmatter, 'date' | 'title' | 'category' | 'summary'> {
+  extends Pick<
+    Queries.MarkdownRemarkFrontmatter,
+    'date' | 'title' | 'category' | 'summary' | 'tags'
+  > {
   thumbnail: string;
   slug: Queries.MarkdownRemarkFields['slug'];
   timeToRead: Queries.MarkdownRemark['timeToRead'];
