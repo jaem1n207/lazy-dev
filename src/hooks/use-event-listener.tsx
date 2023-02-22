2a1d32f849c409b6d9886814dcf438081e71ca31
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-23 03:34:24 +0900

    ✨ feat: use-event-listener hook

---
+++ src/hooks/use-event-listener.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+import { useRef, useEffect } from 'react';
+
+type Options = Pick<AddEventListenerOptions, 'capture' | 'passive' | 'once'>;
+
+interface UseEventListener<K extends keyof DocumentEventMap> {
+  (eventName: K, handler: DocumentEventMap[K], element?: Document, options?: Options): void;
+}
+
+export const useEventListener: UseEventListener<keyof DocumentEventMap> = (
+  eventName,
+  handler,
+  element = document,
+  options = {}
+) => {
+  const savedHandler = useRef<DocumentEventMap[keyof DocumentEventMap]>();
+  const { capture, passive, once } = options;
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const isSupported = element && element.addEventListener;
+    if (!isSupported) {
+      return;
+    }
+
+    const eventListener = (event: DocumentEventMap[keyof DocumentEventMap]) =>
+      // @ts-ignore
+      savedHandler.current?.(event);
+    const opts = { capture, passive, once };
+    element.addEventListener(eventName, eventListener, opts);
+    return () => {
+      element.removeEventListener(eventName, eventListener, opts);
+    };
+  }, [eventName, element, capture, passive, once]);
+};
