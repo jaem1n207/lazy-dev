2407aef3976a19b7b48c2dae759012961957f13b
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-23 11:51:48 +0900

    :bug: node 환경에서 build할 때 생기는 에러 대응

--- src/components/cursor-follower.tsx
+++ src/components/cursor-follower.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import useCursor from 'Hooks/use-cursor';
+import { isBrowser } from 'Libs/environment';
 
 const CursorFollower = () => {
   const { styles, cursorInnerRef, cursorOuterRef } = useCursor();
+
+  if (!isBrowser) {
+    return null;
+  }
+
   return (
     <>
       <div ref={cursorInnerRef} style={styles.cursorInner} />
