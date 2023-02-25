f22d5159ed4ece36832db57f8c7fa92ec0868f19
jaem1n207 <roy.jm.lee@gmail.com>
2023-02-25 14:36:37 +0900

    :zap: web woff2 font 미리 로드하고 캐싱

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -13,31 +13,70 @@ export const onRenderBody: GatsbySSR['onRenderBody'] = ({
       name="google-site-verification"
       content="ohUVyTBdTm7pjIAXpNjQ9EppEzdrbFijShnU7Q0GEJI"
     />,
+    <link
+      key="preconnect-google-fonts"
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      // @ts-ignore
+      crossOrigin="true"
+    />,
+    <link
+      key="preload-notosanskr-regular"
+      rel="preload"
+      as="font"
+      type="font/woff2"
+      href="/fonts/NotoSansKr-Regular.woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="preload-notosanskr-bold"
+      rel="preload"
+      as="font"
+      type="font/woff2"
+      href="/fonts/NotoSansKr-Bold.woff2"
+      crossOrigin="anonymous"
+    />,
+    <style key="local-fonts" type="text/css">
+      {`
+        @font-face {
+          font-family: 'Noto Sans KR';
+          font-weight: 400;
+          src: url('/fonts/NotoSansKr-Regular.woff2') format('woff2');
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Noto Sans KR';
+          font-weight: 700;
+          src: url('/fonts/NotoSansKr-Bold.woff2') format('woff2');
+          font-display: swap;
+        }
+      `}
+    </style>,
     <script
       key="darkmode"
       dangerouslySetInnerHTML={{
-        __html: `(function() {  
-            function setTheme(theme) {
-              window.__theme = theme;
-              if (theme === 'dark') {
-                document.documentElement.className = 'dark';
-              } else {
-                document.documentElement.className = '';
-              }
-            };
-            window.__setPreferredTheme = function(theme) {
-              setTheme(theme);
-              try {
-                localStorage.setItem('color-theme', theme);
-              } catch (e) {}
-            };
-            let preferredTheme;
+        __html: `(function() {
+          function setTheme(theme) {
+            window.__theme = theme;
+            if (theme === 'dark') {
+              document.documentElement.className = 'dark';
+            } else {
+              document.documentElement.className = '';
+            }
+          };
+          window.__setPreferredTheme = function(theme) {
+            setTheme(theme);
             try {
-              preferredTheme = localStorage.getItem('color-theme');
+              localStorage.setItem('color-theme', theme);
             } catch (e) {}
-            let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-            setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
-          })();`,
+          };
+          let preferredTheme;
+          try {
+            preferredTheme = localStorage.getItem('color-theme');
+          } catch (e) {}
+          let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+          setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+        })();`,
       }}
     />,
   ]);
