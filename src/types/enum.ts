8b86256c4ad862be51455a683ebcf08c4baaca2c
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-14 15:31:44 +0900

    :recycle: CLICKABLE 요소 모듈 레벨로 관리

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -11,3 +11,8 @@ export const ROUTES = {
     toUrl: (category: string) => `${ROUTES.HOME}?category=${category}`,
   },
 };
+
+export const ELEMENT_SELECTOR = {
+  CLICKABLE:
+    'details, summary, a, input[type="submit"], input[type="image"], label[for], select, button:not([disabled]), .link, input[type="text"], input[type="checkbox"]:not([disabled]), input[type="radio"]:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])',
+};
