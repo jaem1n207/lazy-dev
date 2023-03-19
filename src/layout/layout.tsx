1a1cf4c32fb0fdb55a1f1ea20ccb0e4769649a4d
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-19 23:24:27 +0900

    ✨ feat: custom cursor 컴포넌트 및 use-hover 훅 레이아웃 컴포넌트에 적용

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,9 +1,10 @@
-import React, { ElementType, ReactNode } from 'react';
+import React, { ElementType, ReactNode, useEffect } from 'react';
 
 import { Slice } from 'gatsby';
 import tw, { GlobalStyles as BaseStyles } from 'twin.macro';
 
-import CursorFollower from 'Components/cursor-follower';
+import CustomCursor from 'Components/CustomCursor';
+import { useHover } from 'Hooks/use-hover';
 
 interface LayoutProps {
   children: ReactNode;
@@ -20,16 +21,37 @@ const Layout = ({ children, location, title, as = 'div' }: LayoutProps) => {
 
   const As = as;
 
+  const { ref: cursorRef, isHovered: isCursorHovered } = useHover();
+
+  useEffect(() => {
+    const clickableElements = document.querySelectorAll('ul, a, button, input');
+    clickableElements.forEach((element) => {
+      console.log(element);
+      element.classList.add('circle-active');
+    });
+
+    return () => {
+      clickableElements.forEach((element) => {
+        element.classList.remove('circle-active');
+      });
+    };
+  });
+
   return (
-    <As css={[tw`max-w-2xl mx-auto my-0pxr py-40pxr px-20pxr`, isRootPath && tw`mb-80pxr`]}>
-      <BaseStyles />
-      <CursorFollower />
-      <Slice alias="header" size={isRootPath ? 'large' : 'medium'}>
-        {title}
-      </Slice>
-      <main>{children}</main>
-      <Slice alias="footer" />
-    </As>
+    <>
+      <As css={[tw`max-w-2xl mx-auto my-0pxr py-40pxr px-20pxr`, isRootPath && tw`mb-80pxr`]}>
+        <CustomCursor active={isCursorHovered} />
+        <div ref={cursorRef}>
+          <BaseStyles />
+          {/* <CursorFollower /> */}
+          <Slice alias="header" size={isRootPath ? 'large' : 'medium'}>
+            {title}
+          </Slice>
+          <main>{children}</main>
+          <Slice alias="footer" />
+        </div>
+      </As>
+    </>
   );
 };
 
