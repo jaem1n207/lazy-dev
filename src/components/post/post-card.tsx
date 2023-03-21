a811d435246ffaf37559577fe03e93c259d38c8f
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-22 02:31:01 +0900

    ✨ feat: post card 컴포넌트 작성

--- src/components/post/post-card.tsx
+++ src/components/post/post-card.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import { GatsbyImage, GatsbyImageProps } from 'gatsby-plugin-image';
+
+import { H3, H6 } from 'Components/common';
+import { ROUTES } from 'Types/enum';
+import Post from 'Types/post';
+
+interface PostCardProps {
+  post: Post;
+}
+
+const PostCard = ({
+  post: { slug, title, summary, date, category, thumbnail, timeToRead },
+}: PostCardProps) => {
+  const data = useStaticQuery<Queries.Query>(graphql`
+    query CenteredImage {
+      allImageSharp {
+        edges {
+          node {
+            id
+            gatsbyImageData(formats: WEBP)
+          }
+        }
+      }
+    }
+  `);
+
+  const image = useMemo(() => {
+    const image = data.allImageSharp.edges.find((edge) => edge.node.id === thumbnail);
+
+    return image?.node.gatsbyImageData as GatsbyImageProps['image'];
+  }, [data.allImageSharp.edges, thumbnail]);
+
+  if (!slug) return null;
+
+  return (
+    <article className="relative w-full">
+      <Link
+        to={ROUTES.BLOG_POST.toUrl(slug)}
+        className="relative block w-full group peer focus:outline-none"
+      >
+        <div className="rounded-lg aspect-h-4 aspect-w-3">
+          <GatsbyImage
+            image={image}
+            alt={title ?? 'post thumbnail'}
+            loading="lazy"
+            className="object-cover object-center w-full transition rounded-lg"
+          />
+        </div>
+        <div className="mt-32pxr text-20pxr">
+          <time dateTime={date!}>{date}</time> — {timeToRead} min read
+        </div>
+        <H3 as="div" className="mt-16pxr">
+          {title}
+        </H3>
+        <H6 variant="secondary" as="div" className="truncate mt-8pxr">
+          {summary}
+        </H6>
+      </Link>
+    </article>
+  );
 };
 
 export default PostCard;
