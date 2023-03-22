e9601395c1f590180f050e10ffa8b795aad70fe2
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-23 01:51:21 +0900

    ✨ feat: isNil, isEmptyArray assertions

--- src/libs/assertions.ts
+++ src/libs/assertions.ts
@@ -1,1 +1,12 @@
-export const isEmptyString = (str: string) => str === '';
+export const isNil = (value: unknown): value is null | undefined => {
+  return value === null || value === undefined;
+};
+
+export const isEmptyString = (value: unknown): value is '' => {
+  return isNil(value) || value?.toString().trim() === '';
+};
+
+export const isEmptyArray = (value: unknown): value is [] => {
+  // @ts-ignore
+  return isNil(value) || value?.length === 0;
+};
