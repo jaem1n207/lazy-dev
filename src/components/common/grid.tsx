ddd9ba06d1454357733d60a7d2df246ec16c0cca
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-22 18:42:52 +0900

    :recycle: grid renderContentSpacer props 제거 및 스타일 수정

--- src/components/common/grid.tsx
+++ src/components/common/grid.tsx
@@ -3,32 +3,18 @@ import React, { ElementType, forwardRef, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import { motion, MotionProps } from 'framer-motion';
 
-import ContentSpacer from './content-spacer';
-
 interface GridProps extends HTMLAttributes<HTMLElement> {
   as?: ElementType;
   className?: string;
   rowGap?: string;
-  renderContentSpacer?: boolean;
   children: React.ReactNode;
   animated?: boolean;
 }
 
 const Grid = forwardRef<HTMLElement, GridProps>(
-  (
-    {
-      as: Component = 'div',
-      className,
-      rowGap,
-      renderContentSpacer = true,
-      children,
-      animated = true,
-      ...rest
-    },
-    ref
-  ) => {
+  ({ as: Component = 'div', className, rowGap, children, animated = true, ...rest }, ref) => {
     const baseStyles = classNames(
-      `relative grid grid-cols-12 gap-x-24pxr tablet:grid-cols-8 tablet:gap-x-16pxr desktop:grid-cols-4`,
+      `max-w-7xl mx-auto relative grid grid-cols-12 gap-x-24pxr tablet:gap-x-16pxr desktop:grid-cols-4`,
       {
         [`grid-row-gap-${rowGap}`]: rowGap,
       },
@@ -45,15 +31,9 @@ const Grid = forwardRef<HTMLElement, GridProps>(
 
     return (
       <motion.div {...motionProps}>
-        {renderContentSpacer ? (
-          <ContentSpacer ref={ref} {...rest}>
-            <Component className={baseStyles}>{children}</Component>
-          </ContentSpacer>
-        ) : (
-          <Component ref={ref} className={baseStyles} {...rest}>
-            {children}
-          </Component>
-        )}
+        <Component ref={ref} className={baseStyles} {...rest}>
+          {children}
+        </Component>
       </motion.div>
     );
   }
