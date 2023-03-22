7cf96a1e64a873fc28e485d338027142f49e1b53
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-23 00:53:25 +0900

    💎 style: footer max width

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,15 +4,17 @@ import { ContentSpacer, UnderlineLink } from 'Components/common';
 
 const Footer = () => {
   return (
-    <ContentSpacer as="footer" className="flex mt-32pxr pt-28pxr">
-      @ {new Date().getFullYear()}, Built with&nbsp;
-      <UnderlineLink external url="https://github.com/jaem1n207/lazy-dev">
-        Lazy Dev
-      </UnderlineLink>
-      <div className="ml-auto">
-        <UnderlineLink external url="/rss.xml">
-          rss
+    <ContentSpacer as="footer" className="mt-32pxr pt-28pxr">
+      <div className="flex mx-auto max-w-7xl">
+        @ {new Date().getFullYear()}, Built with&nbsp;
+        <UnderlineLink external url="https://github.com/jaem1n207/lazy-dev">
+          Lazy Dev
         </UnderlineLink>
+        <div className="ml-auto">
+          <UnderlineLink external url="/rss.xml">
+            rss
+          </UnderlineLink>
+        </div>
       </div>
     </ContentSpacer>
   );
