e8bb2cb7bcd61a7d4b7638d9feca0ff3ae35f024
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-24 02:48:55 +0900

    🐛 fix: tailwind 스타일 우선순위 문제 해결

--- src/components/common/content-spacer.tsx
+++ src/components/common/content-spacer.tsx
@@ -1,7 +1,7 @@
 import React, { ElementType, HTMLAttributes, forwardRef } from 'react';
 
 import { window } from 'browser-monads-ts';
-import classNames from 'classnames';
+import tw from 'twin.macro';
 
 import { checkRootPath } from 'Libs/url';
 
@@ -16,17 +16,17 @@ const ContentSpacer = forwardRef<HTMLElement, ContentSpacerProps>(
   ({ as: Component = 'div', className, children, compact = false, ...rest }, ref) => {
     const isRootPath = checkRootPath(window.location.pathname);
 
-    const baseStyles = classNames(
-      `relative mx-10vw`,
-      {
-        'mx-auto max-w-7xl': compact,
-        'mx-auto max-w-4xl': !isRootPath,
-      },
-      className
-    );
-
     return (
-      <Component ref={ref} className={baseStyles} {...rest}>
+      <Component
+        ref={ref}
+        css={[
+          tw`relative mx-10vw`,
+          compact && tw`mx-auto max-w-7xl`,
+          !isRootPath && tw`max-w-4xl mx-auto`,
+          ...(className ? [className] : []),
+        ]}
+        {...rest}
+      >
         {children}
       </Component>
     );
