a59c0be7fc58c6c8105bf846e49e41d0dca732e8
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-27 17:39:32 +0900

    :zap: post card thumbnail image data

--- src/components/post/post-card.tsx
+++ src/components/post/post-card.tsx
@@ -21,10 +21,11 @@ const PostCard = ({
           node {
             id
             gatsbyImageData(
-              width: 900
               placeholder: BLURRED
               formats: [AUTO, WEBP, AVIF]
               aspectRatio: 1
+              sizes: "(max-width:639px) 80vw','(min-width:640px) and (max-width:1023px) 40vw','(min-width:1024px) and (max-width:1620px) 25vw','420px'"
+              breakpoints: [280, 560, 840, 1100, 1300, 1650]
             )
           }
         }
