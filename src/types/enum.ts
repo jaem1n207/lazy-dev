039c975abe7517f782661576a445fb07df82758d
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-29 19:02:13 +0900

    :recycle: ELEMENT_SELECTOR 요소 관리 용이하도록 수정

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -12,7 +12,45 @@ export const ROUTES = {
   },
 };
 
+export const ELEMENT_CLASS = {
+  NONE_ACTIVE: 'none-active',
+};
+
+const ELEMENT_PARTS = {
+  details: 'details:not([disabled])',
+  summary: 'summary:not([disabled])',
+  anchor: 'a:not([disabled])',
+  submit: 'input[type="submit"]:not([disabled])',
+  image: 'input[type="image"]:not([disabled])',
+  select: 'select:not([disabled])',
+  button: 'button:not([disabled])',
+  link: '.link:not([disabled])',
+  text: 'input[type="text"]:not([disabled])',
+  checkbox: 'input[type="checkbox"]:not([disabled])',
+  radio: 'input[type="radio"]:not([disabled])',
+  textarea: 'textarea:not([disabled])',
+  tabindex: '[tabindex]:not([tabindex="-1"]):not([disabled])',
+  label: 'label:not([disabled])',
+};
+
+const ANIMATE_ELEMENT_PARTS = {
+  details: `details:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  summary: `summary:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  anchor: `a:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  submit: `input[type="submit"]:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  image: `input[type="image"]:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  select: `select:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  button: `button:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  link: `.link:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  text: `input[type="text"]:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  checkbox: `input[type="checkbox"]:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  radio: `input[type="radio"]:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  textarea: `textarea:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  tabindex: `[tabindex]:not([tabindex="-1"]):not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+  label: `label:not(.${ELEMENT_CLASS.NONE_ACTIVE}):not([disabled])`,
+};
+
 export const ELEMENT_SELECTOR = {
-  CLICKABLE:
-    'details, summary, a, input[type="submit"], input[type="image"], label[for], select, button:not([disabled]), .link, input[type="text"], input[type="checkbox"]:not([disabled]), input[type="radio"]:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])',
+  CLICKABLE: Object.values(ELEMENT_PARTS).join(', '),
+  ANIMATE: Object.values(ANIMATE_ELEMENT_PARTS).join(', '),
 };
