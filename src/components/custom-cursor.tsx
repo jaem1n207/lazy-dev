949403dad1a8a928d736717f5d239df348dadd44
jaem1n207 <roy.jm.lee@gmail.com>
2023-03-31 18:33:01 +0900

    :zap: custom  cursor 리팩토링

--- src/components/custom-cursor.tsx
+++ src/components/custom-cursor.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
-
-import useCursor2 from 'Hooks/use-cursor2';
+import React, { useEffect, useRef } from 'react';
 
 const CustomCursor = () => {
-  const cursorRef = useCursor2();
+  const cursorRef = useRef<HTMLDivElement | null>(null);
 
-  return <div ref={cursorRef} className="custom-cursor" />;
+  useEffect(() => {
+    const isElementInteractive = (el: Element | null): boolean => {
+      const interactiveElements = [
+        'A',
+        'BUTTON',
+        'INPUT',
+        'TEXTAREA',
+        'SELECT',
+        'LABEL',
+        'VIDEO',
+        'AUDIO',
+        'IMG',
+      ];
 
-  // const { styles, cursorRef, shouldRender } = useCursor();
+      if (!el) return false;
 
-  // if (!shouldRender) return null;
+      if (interactiveElements.includes(el.tagName)) return true;
 
-  // return <div ref={cursorRef} className="custom-cursor" style={styles.cursor} />;
+      return false;
+    };
+
+    const updateCursor = (x: number, y: number, hoveredElement: Element | null) => {
+      if (!cursorRef.current) return;
+
+      if (isElementInteractive(hoveredElement)) {
+        cursorRef.current.style.transform = `translate(-50%, -50%) translate(${x}px, ${y}px)`;
+      } else {
+        cursorRef.current.style.transform = `translate(-50%, -50%) translate(${x}px, ${y}px) scale(0.23, 0.23)`;
+      }
+    };
+
+    const onMouseMove = (e: MouseEvent) => {
+      const x = e.clientX;
+      const y = e.clientY;
+      const hoveredElement = document.elementFromPoint(x, y);
+
+      requestAnimationFrame(() => {
+        updateCursor(x, y, hoveredElement);
+      });
+    };
+
+    document.body.addEventListener('mousemove', onMouseMove);
+
+    return () => {
+      document.body.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
+
+  return <div ref={cursorRef} className="custom-cursor"></div>;
 };
 
 export default CustomCursor;
