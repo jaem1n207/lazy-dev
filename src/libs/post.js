cc679050d62fe635718b332eeba73c6a4a28bdd0
jaem1n207 <roy.jm.lee@gmail.com>
2023-04-19 11:35:48 +0900

    :recycle: refactor post script

--- src/libs/post.js
+++ src/libs/post.js
@@ -13,21 +13,10 @@ const log = makeCustomSignale();
 const interactiveLog = makeInteractiveSignale();
 
 const BLOG_DIR_PATH = path.join(process.cwd(), 'content', 'blog');
+const THUMBNAIL_TARGET_DIR = path.join(BLOG_DIR_PATH, 'thumbnails');
 const IGNORE_DIR = 'images';
 const AUTHOR_ID = 'jaemin';
-
-let step = 1;
-const totalStep = 4;
-
-/**
- *
- * @param {string} file
- * @param {fs.Stats} stats
- * @returns {boolean} object returned from `fs.lstat()`
- */
-const ignoreFunc = (file, stats) => stats.isDirectory() && path.basename(file) === IGNORE_DIR;
-
-const uniq = (arr) => [...new Set(arr)];
+const CUSTOM_CATEGORY_OPTIONS = ['[ CREATE NEW CATEGORY ]', '[ CANCEL ]'];
 
 /**
  *
@@ -38,16 +27,13 @@ const getFileName = (title) => title.trim().toLowerCase().replace(/ /g, '-'); //
 
 /**
  *
- * @param {object} rawContents
- * @param {string} rawContents.title
- * @param {string} rawContents.date
- * @param {string} rawContents.category
- * @param {string} rawContents.thumbnail
- * @param {string} rawContents.summary
- * @returns {string} refined contents
+ * @param {string} file
+ * @param {fs.Stats} stats
+ * @returns {boolean} object returned from `fs.lstat()`
  */
-const refineContents = (rawContents) =>
-  matter.stringify('', rawContents).split("'").join('"').replace(/\\n/g, ' '); // replace single quote to double quote
+const ignoreFunc = (file, stats) => stats.isDirectory() && path.basename(file) === IGNORE_DIR;
+
+const uniq = (arr) => [...new Set(arr)];
 
 /**
  *
@@ -73,94 +59,50 @@ const getCategory = async (targetDir) => {
 /**
  *
  * @param {string} targetDir
- * @returns {Promise<string>} category
+ * @returns {string[]} files in the target directory
  */
-const fetchCategory = async (targetDir) => {
-  const folderNameRegex = /^(\w+\.?)*\w+$/;
+const getFilesOfDir = (targetDir) => fs.readdirSync(targetDir);
 
-  let category = null;
-  const customCategoryOptions = ['[ CREATE NEW CATEGORY ]', '[ CANCEL ]'];
+const isNil = (value) => {
+  return value === null || value === undefined;
+};
 
-  const categories = await getCategory(targetDir);
+const isEmptyString = (value) => {
+  return isNil(value) || value?.toString().trim() === '';
+};
 
-  interactiveLog.await(`[%d/%d] - 카테고리를 선택하는 중...`, step, totalStep);
+const getContentsInquirer = async () => {
+  const categories = await getCategory(BLOG_DIR_PATH);
   const categoryInquirer = [
     {
       type: 'list',
       name: 'selectedCategory',
       message: '카테고리 선택: ',
-      choices: [...customCategoryOptions, new inquirer.Separator(), ...categories],
+      choices: [...CUSTOM_CATEGORY_OPTIONS, new inquirer.Separator(), ...categories],
     },
   ];
 
-  const { selectedCategory } = await inquirer.prompt(categoryInquirer);
-
-  if (selectedCategory === customCategoryOptions[0]) {
-    const { newCategory } = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'newCategory',
-        message: '새 카테고리 입력: ',
-        validate: (input) => {
-          if (!input) {
-            return '새 카테고리를 입력해주세요';
-          }
-
-          if (categories.includes(input.toLowerCase())) {
-            return `${input} 카테고리는 이미 존재합니다. 다른 이름을 입력해주세요`;
-          }
-
-          if (input.length < 2) {
-            return '카테고리 이름이 너무 짧습니다. 최소 2자 이상이어야 합니다';
-          }
-
-          if (!folderNameRegex.test(input)) {
-            return '카테고리 이름이 잘못되었습니다. 영문, 숫자, ., _ 만 사용할 수 있습니다';
-          }
-
-          return true;
-        },
-      },
-    ]);
-
-    category = newCategory.toLowerCase();
-  } else if (selectedCategory === customCategoryOptions[1]) {
-    interactiveLog.error(`[%d/%d] - 카테고리 선택이 취소되었습니다`, step, totalStep);
-    process.exit(0);
-  }
-
-  step++;
-
-  return category || selectedCategory;
-};
-
-/**
- *
- * @param {string} targetDir
- * @param {string} category
- * @returns {Promise<string>} title
- */
-const fetchTitle = async (targetDir, category) => {
-  interactiveLog.await(`[%d/%d] - 제목을 입력하는 중...`, step, totalStep);
-  const titleInquirer = [
+  const folderNameRegex = /^(\w+\.?)*\w+$/;
+  const newCategoryInquirer = [
     {
       type: 'input',
-      name: 'title',
-      message: '제목 입력: ',
-      validate: async (input) => {
-        if (!input) {
-          return '제목을 입력해주세요';
+      name: 'newCategory',
+      message: '새 카테고리 입력: ',
+      validate: (input) => {
+        if (isEmptyString(input)) {
+          return '새 카테고리를 입력해주세요';
         }
 
-        if (input.includes("'")) {
-          return '제목에는 작은 따옴표를 사용할 수 없습니다';
+        if (categories.includes(input.toLowerCase())) {
+          return `${input} 카테고리는 이미 존재해요. 다른 이름을 입력해주세요`;
         }
 
-        const fileName = getFileName(input);
-        const isExist = await fs.pathExists(path.join(targetDir, category, `${fileName}.md`));
+        if (input.length < 2) {
+          return '카테고리 이름이 너무 짧아요. 최소 2자 이상이어야 해요';
+        }
 
-        if (isExist) {
-          return `${input} 제목은 이미 ${category} 카테고리에 존재합니다. 다른 제목을 입력해주세요`;
+        if (!folderNameRegex.test(input)) {
+          return '카테고리 이름으로 포함할 수 없는 문자가 있어요. 영문, 숫자, ., _ 만 사용할 수 있어요';
         }
 
         return true;
@@ -168,31 +110,45 @@ const fetchTitle = async (targetDir, category) => {
     },
   ];
 
-  const { title } = await inquirer.prompt(titleInquirer);
+  /**
+   * @param {string} category To verify that the title you entered exists in the selected category
+   */
+  const titleInquirer = (category) => {
+    return [
+      {
+        type: 'input',
+        name: 'title',
+        message: '제목 입력: ',
+        validate: (input) => {
+          if (isEmptyString(input)) {
+            return '제목을 입력해주세요';
+          }
 
-  step++;
+          if (input.includes("'")) {
+            return '제목에는 작은 따옴표를 사용할 수 없어요';
+          }
 
-  return title;
-};
+          const fileName = getFileName(input);
+          const isExist = fs.pathExistsSync(path.join(BLOG_DIR_PATH, category, `${fileName}.md`));
+          if (isExist) {
+            return `${input} 제목은 이미 ${category} 카테고리에 존재해요. 다른 제목을 입력해주세요`;
+          }
 
-/**
- *
- * @param {string} targetDir
- * @returns {string[]} files in the target directory
- */
-const getFilesOfDir = (targetDir) => fs.readdirSync(targetDir);
+          return true;
+        },
+      },
+    ];
+  };
 
-/**
- *
- * @param {string} targetDir
- * @returns {Promise<string>} thumbnail
- */
-const fetchThumbnail = async (targetDir) => {
-  interactiveLog.await(`[%d/%d] - 썸네일을 입력하는 중...`, step, totalStep);
+  const tagInquirer = [
+    {
+      type: 'input',
+      name: 'tag',
+      message: '태그 입력 (태그 입력을 끝내려면 빈 문자열을 입력해주세요): ',
+    },
+  ];
 
-  let thumbnail = null;
-  const thumbnails = getFilesOfDir(targetDir);
-
+  const thumbnails = getFilesOfDir(THUMBNAIL_TARGET_DIR);
   const thumbnailInquirer = [
     {
       type: 'list',
@@ -202,26 +158,13 @@ const fetchThumbnail = async (targetDir) => {
     },
   ];
 
-  const { selectedThumbnail } = await inquirer.prompt(thumbnailInquirer);
-
-  thumbnail = path.relative(BLOG_DIR_PATH, path.join(targetDir, selectedThumbnail));
-  thumbnail = `../${thumbnail}`;
-
-  step += 1;
-
-  return thumbnail;
-};
-
-const fetchSummary = async () => {
-  interactiveLog.await(`[%d/%d] - 요약을 입력하는 중...`, step, totalStep);
-
   const summaryInquirer = [
     {
       type: 'input',
       name: 'summary',
       message: '요약 입력: ',
       validate: (input) => {
-        if (!input) {
+        if (isEmptyString(input)) {
           return '요약을 입력해주세요';
         }
 
@@ -230,19 +173,96 @@ const fetchSummary = async () => {
     },
   ];
 
-  const { summary } = await inquirer.prompt(summaryInquirer);
+  return {
+    categoryInquirer: {
+      selected: categoryInquirer,
+      new: newCategoryInquirer,
+    },
+    titleInquirer,
+    tagInquirer,
+    thumbnailInquirer,
+    summaryInquirer,
+  };
+};
 
+const promptContents = async () => {
+  const contentsInquirer = await getContentsInquirer();
+
+  let step = 1;
+  let totalSteps = Object.keys(contentsInquirer).length;
+
+  interactiveLog.await(`[%d/%d] - 카테고리를 선택하는 중이예요...`, step, totalSteps);
+  let category = null;
+  const { selectedCategory } = await inquirer.prompt(contentsInquirer.categoryInquirer.selected);
+  if (selectedCategory === CUSTOM_CATEGORY_OPTIONS[0]) {
+    const { newCategory } = await inquirer.prompt(contentsInquirer.categoryInquirer.new);
+    category = newCategory.toLowerCase();
+  } else if (selectedCategory === CUSTOM_CATEGORY_OPTIONS[1]) {
+    interactiveLog.error(`[%d/%d] - 카테고리 선택이 취소되었어요`, step, totalSteps);
+    process.exit(0);
+  }
+  const categoryValue = category || selectedCategory;
   step++;
 
-  return summary;
+  interactiveLog.await(`[%d/%d] - 제목을 입력하는 중이예요...`, step, totalSteps);
+  const { title } = await inquirer.prompt(contentsInquirer.titleInquirer(categoryValue));
+  step++;
+
+  interactiveLog.await(`[%d/%d] - 태그를 입력하는 중이예요...`, step, totalSteps);
+  const tags = [];
+  const maxTagCount = 10;
+  for (let i = 0; i < maxTagCount; i++) {
+    const { tag } = await inquirer.prompt(contentsInquirer.tagInquirer);
+    if (isEmptyString(tag)) {
+      break;
+    }
+
+    tags.push(tag);
+  }
+  step++;
+
+  interactiveLog.await(`[%d/%d] - 썸네일을 선택하는 중이예요...`, step, totalSteps);
+  let thumbnail = null;
+  const { selectedThumbnail } = await inquirer.prompt(contentsInquirer.thumbnailInquirer);
+  thumbnail = path.relative(BLOG_DIR_PATH, path.join(THUMBNAIL_TARGET_DIR, selectedThumbnail));
+  thumbnail = `../${thumbnail}`;
+  step++;
+
+  interactiveLog.await(`[%d/%d] - 요약을 입력하는 중이예요...`, step, totalSteps);
+  const { summary } = await inquirer.prompt(contentsInquirer.summaryInquirer);
+  step++;
+
+  return {
+    category: categoryValue,
+    title,
+    tags,
+    thumbnail,
+    summary,
+  };
 };
+
+/**
+ *
+ * @param {object} rawContents
+ * @param {string} rawContents.title
+ * @param {string} rawContents.date
+ * @param {string} rawContents.category
+ * @param {string} rawContents.authorId
+ * @param {string} rawContents.thumbnail
+ * @param {string} rawContents.summary
+ * @returns {string} refined contents
+ */
+const refineContents = (rawContents) =>
+  matter.stringify('', rawContents).split("'").join('"').replace(/\\n/g, ' '); // replace single quote to double quote
 
 module.exports = (async function () {
   console.log(''); // empty line
 
+  const { category, title, tags, thumbnail, summary } = await promptContents();
   const date = dayjs().format('YYYY-MM-DD HH:mm:ss');
+  const draft = false;
+  const fileName = getFileName(title);
 
-  const category = await fetchCategory(BLOG_DIR_PATH);
   const destDir = path.join(BLOG_DIR_PATH, category);
   const destDirExists = await fs.pathExists(destDir);
 
@@ -250,14 +270,12 @@ module.exports = (async function () {
     await fs.mkdirp(destDir);
   }
 
-  const title = await fetchTitle(BLOG_DIR_PATH, category);
-  const fileName = getFileName(title);
-  const thumbnail = await fetchThumbnail(path.join(BLOG_DIR_PATH, 'thumbnails'));
-  const summary = await fetchSummary();
   const contents = refineContents({
     title,
     date,
     category,
+    tags,
+    draft,
     authorId: AUTHOR_ID,
     thumbnail,
     summary,
