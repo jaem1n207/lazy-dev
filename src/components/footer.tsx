4c5503f3adfdc0db81098e5f4a00dd0d1ee26b42
jaem1n207 <roy.jm.lee@gmail.com>
2023-05-24 17:51:20 +0900

    :memo: 푸터 텍스트 변경

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
     <ContentSpacer as="footer" className="pb-32pxr pt-28pxr">
       <div className="flex mx-auto max-w-7xl">
         @ {new Date().getFullYear()}, Built with&nbsp;
-        <UnderlineLink external url="https://github.com/jaem1n207/lazy-dev">
-          Lazy Dev
+        <UnderlineLink external url="https://www.gatsbyjs.com/">
+          Gatsby
         </UnderlineLink>
         <div className="ml-auto">
           <UnderlineLink external url="/rss.xml">
