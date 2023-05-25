37e8b543f7646f369c2cce24cf8200b361b2281d
jaem1n207 <roy.jm.lee@gmail.com>
2023-05-25 16:37:28 +0900

    :memo: 푸터 텍스트 변경

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <ContentSpacer as="footer" className="pb-32pxr pt-28pxr">
       <div className="flex mx-auto max-w-7xl">
         @ {new Date().getFullYear()}, Built with&nbsp;
-        <UnderlineLink external url="https://www.gatsbyjs.com/">
+        <UnderlineLink external url="https://www.gatsbyjs.com">
           Gatsby
         </UnderlineLink>
         <div className="ml-auto">
