c6f4d8488534e814dda5cf2f2a41b7c6f8983dc9
jaem1n207 <roy.jm.lee@gmail.com>
2023-05-26 18:22:41 +0900

    :sparkles: Browser api 함수 타입 지정

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -44,11 +44,11 @@ const withCustomCursor = (Component: React.ComponentType) => {
   return CustomCursorWrapper;
 };
 
-export const wrapPageElement = ({ element, props }: any) => {
-  return React.createElement(withCustomCursor(element.type), props);
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element, props }) => {
+  return React.createElement(withCustomCursor(element.props.type || 'div'), props, element);
 };
 
-// const UPDATE_SCROLL_TIME_OUT = 1;
+const UPDATE_SCROLL_TIME_OUT = 1;
 
 /**
  * 블로그 상세 페이지에서 뒤로가기를 눌렀을 때 스크롤 위치를 유지하기 위한 설정
@@ -56,17 +56,17 @@ export const wrapPageElement = ({ element, props }: any) => {
  * 블로그 홈으로 이동되서 잠깐 깜빡이는 문제를 해결하기 위함
  * @see https://github.com/gatsbyjs/gatsby/issues/28794#issuecomment-905173663
  */
-// export const shouldUpdateScroll: GatsbyBrowser['shouldUpdateScroll'] = ({
-//   routerProps: { location },
-//   getSavedScrollPosition,
-// }) => {
-//   window.history.scrollRestoration = 'manual';
-//   const currentPosition = getSavedScrollPosition(location);
-//   window.setTimeout(() => {
-//     window.scrollTo(...currentPosition);
-//   }, UPDATE_SCROLL_TIME_OUT);
-//   return false;
-// };
+export const shouldUpdateScroll: GatsbyBrowser['shouldUpdateScroll'] = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  window.history.scrollRestoration = 'manual';
+  const currentPosition = getSavedScrollPosition(location);
+  window.setTimeout(() => {
+    window.scrollTo(...currentPosition);
+  }, UPDATE_SCROLL_TIME_OUT);
+  return false;
+};
 
 export const onServiceWorkerUpdateReady: GatsbyBrowser['onServiceWorkerUpdateReady'] = () => {
   const answer = window.confirm(
