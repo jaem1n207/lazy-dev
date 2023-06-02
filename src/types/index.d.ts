594911bfc20ee9eeeb3f1c744bd2ab664ab424f9
jaem1n207 <roy.jm.lee@gmail.com>
2023-06-02 14:18:39 +0900

    :wrench: Emotion 타입

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,3 @@
-import { TwStyle } from 'twin.macro';
-
 // TypeScript 컴파일러는 고유의 컴파일 규칙을 따라서 export/import 구문이 없는 파일을 모듈이 아닌 스크립트로 취급함
 // 가짜 export를 넣어서 외부 모듈로 인식되게 함
 export {};
@@ -13,9 +11,3 @@ declare global {
 }
 
 declare const __PATH_PREFIX__: string;
-
-declare module 'gatsby' {
-  interface GatsbyLinkProps {
-    css?: TwStyle;
-  }
-}
