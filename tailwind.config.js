0c52b69e88c8b4102ef64a82fd5695e566b7226a
jaem1n207 <roy.jm.lee@gmail.com>
2023-08-18 18:03:19 +0900

    :bug: Tailwindcss 로드되지 않는 현상 개선

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   darkMode: 'class',
   content: [
+    './gatsby-ssr.tsx',
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/layout/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
