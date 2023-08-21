7f4fca236992253fcf18391ad8d82e72c4a779da
jaem1n207 <roy.jm.lee@gmail.com>
2023-08-22 02:12:15 +0900

    💎 style: motion animation 정의

---
+++ src/utils/motion.ts
@@ -0,0 +1,30 @@
+type Direction = 'left' | 'right' | 'up' | 'down';
+type Type = 'tween' | 'spring' | 'just' | 'keyframes' | 'inertia' | 'decay' | 'none';
+
+interface FadeIn {
+  direction: Direction;
+  type: Type;
+  delay: number;
+  duration: number;
+}
+
+export const fadeIn = ({ direction, type, delay, duration }: FadeIn) => {
+  return {
+    hidden: {
+      x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+      y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};
