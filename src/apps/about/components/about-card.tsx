c9fa00436083e6f3f4a322bf1df9a34431c98d60
jaem1n207 <roy.jm.lee@gmail.com>
2023-08-24 02:17:43 +0900

    :lipstick: 소개카드 틸트 옵션 조정

--- src/apps/about/components/about-card.tsx
+++ src/apps/about/components/about-card.tsx
@@ -14,7 +14,7 @@ const AboutCard = () => {
   return (
     <Tilt
       options={{
-        max: 15,
+        max: 30,
         scale: 1,
         speed: 450,
       }}
@@ -68,7 +68,7 @@ const AboutCard = () => {
       </div>
 
       <div className="flex flex-col justify-center flex-1">
-        <div className="flex items-center justify-center w-4/5 mx-auto font-extrabold tracking-tight text-transparent -mt-24pxr gap-8pxr from-violet via-pink-600 to-primary bg-gradient-to-r bg-clip-text">
+        <div className="flex items-center justify-center w-4/5 mx-auto font-extrabold tracking-tight text-transparent -mt-24pxr gap-8pxr gradient-text">
           <p className="text-40pxr tablet:text-[4vw]">Front-end</p>
           <p className="text-48pxr tablet:text-[6vw]">web Developer</p>
         </div>
