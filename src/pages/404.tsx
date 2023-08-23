828518f3a179f15c7a73e4033b4a4a74e07f2cf4
jaem1n207 <roy.jm.lee@gmail.com>
2023-08-23 11:19:37 +0900

    :sparkles: 404페이지

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { motion } from 'framer-motion';
 import { graphql, HeadFC, navigate, PageProps } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
+import { GoHome } from 'Apps/common/icon/components/svg-icon';
 import Seo from 'Apps/common/seo/seo';
 import { ROUTES } from 'Types/enum';
+import { fadeIn } from 'Utils/motion';
 
 const NotFoundPage: FC<PageProps<Queries.NotFoundQuery>> = () => {
   const goHome = () => {
@@ -16,46 +17,47 @@ const NotFoundPage: FC<PageProps<Queries.NotFoundQuery>> = () => {
   return (
     <div className="flex flex-col items-center justify-center select-none">
       <motion.div
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0 }}
-        className="mb-16pxr"
+        className="mb-16pxr p-12pxr"
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        variants={fadeIn({ direction: 'up', type: 'spring' })}
       >
-        <StaticImage
-          src="../images/not-found.png"
-          alt="Not Found Blog URL"
-          placeholder="blurred"
-          layout="constrained"
-          width={512}
-          className="overflow-hidden"
-        />
+        <StaticImage src="../images/not-found.png" alt="bunny" placeholder="blurred" width={480} />
       </motion.div>
       <motion.h1
-        className="font-bold text-text-primary text-36pxr mb-16pxr"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.2 }}
+        className="font-bold text-text-primary text-36pxr mb-12pxr"
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        variants={fadeIn({ direction: 'up', type: 'spring', delay: 0.2 })}
       >
         앗! 죄송해요.
       </motion.h1>
       <motion.p
-        className="text-center text-text-primary text-18pxr mb-32pxr"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.4 }}
+        className="text-center text-text-primary text-18pxr mb-16pxr"
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        variants={fadeIn({ direction: 'up', type: 'spring', delay: 0.4 })}
       >
         요청하신 페이지를 찾을 수 없어요. <br />
         입력하신 주소가 정확한지 다시 한번 확인해주세요.
       </motion.p>
-      <div>
+      <motion.div
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        variants={fadeIn({ direction: 'left', type: 'spring', delay: 0.6 })}
+      >
         <button
           onClick={goHome}
           className="inline-flex items-center font-medium rounded-md text-text-secondary text-16pxr bg-primary px-16pxr py-8pxr focus-primary"
         >
-          <ArrowLeftIcon className="w-20pxr h-20pxr mr-8pxr" />
+          <GoHome className="w-20pxr h-20pxr mr-8pxr" />
           블로그 홈으로 돌아갈게요.
         </button>
-      </div>
+      </motion.div>
     </div>
   );
 };
