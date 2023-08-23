63ca929731548263807c6c554c6f71ce1ce0c605
jaem1n207 <roy.jm.lee@gmail.com>
2023-08-24 02:18:14 +0900

    :sparkles: Short card 구현

--- src/apps/post/components/short-list.tsx
+++ src/apps/post/components/short-list.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import { Link } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
-
-import { H4 } from 'Apps/common/typography';
-import { ROUTES } from 'Types/enum';
+import ShortCard from './short-card';
 
 interface ShortListProps {
   shorts: Queries.HomeQuery['shorts']['edges'];
@@ -17,26 +13,7 @@ const ShortList = ({ shorts }: ShortListProps) => {
         <h2 className="font-bold text-24pxr foldable:text-20pxr mb-16pxr">Shorts</h2>
         <ul className="flex flex-wrap gap-8pxr">
           {shorts.map((short) => (
-            <li key={short.node.fields?.slug} className="rounded-lg focus-primary">
-              <Link to={ROUTES.BLOG_POST.toUrl(short.node.fields?.slug!)}>
-                <div className="w-full overflow-hidden h-180pxr mb-16pxr desktop:h-160pxr foldable:h-180pxr">
-                  <GatsbyImage
-                    image={short.node.frontmatter?.thumbnail?.childImageSharp?.gatsbyImageData!}
-                    alt={short.node.frontmatter?.title!}
-                    className="w-full h-full rounded-lg"
-                  />
-                </div>
-                <H4 as="h3" className="mb-8pxr">
-                  {short.node.frontmatter?.title}
-                </H4>
-                <time
-                  dateTime={short.node.frontmatter?.date!}
-                  className="text-18pxr foldable:text-16pxr"
-                >
-                  {short.node.frontmatter?.date}
-                </time>
-              </Link>
-            </li>
+            <ShortCard key={short.node.fields?.slug} short={short} />
           ))}
         </ul>
       </div>
