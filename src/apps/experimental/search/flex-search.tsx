28020e21eb2e9a5bbd5962c64ced4a215837169e
jaem1n207 <roy.jm.lee@gmail.com>
2023-11-10 18:02:58 +0900

    :construction: Flexsearch

---
+++ src/apps/experimental/search/flex-search.tsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+
+import FlexSearch from 'flexsearch';
+import { graphql, useStaticQuery } from 'gatsby';
+
+type PageIndex = FlexSearch.Document<
+  {
+    id: number;
+    title: string;
+    content: string;
+  },
+  ['title']
+>;
+
+type SectionIndex = FlexSearch.Document<
+  {
+    id: string;
+    url: string;
+    title: string;
+    content: string;
+  },
+  ['url', 'title', 'content']
+>;
+
+type SearchData = {
+  id: string;
+  title: string;
+  content: string;
+  url: string;
+};
+
+// type SearchData = {
+//   [route: string]: {
+//     title: string;
+//     data: any;
+//   };
+// };
+
+type Result = {
+  _page_rk: number;
+  _section_rk: number;
+  route: string;
+  prefix: React.ReactNode;
+  children: React.ReactNode;
+};
+
+// 캐싱
+const indexes: [PageIndex, SectionIndex] = [];
+
+const useFetchSearchData = async (): Promise<SearchData[]> => {
+  const searchDataQuery = useStaticQuery<Queries.SearchDataQuery>(graphql`
+    query SearchData {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+            rawMarkdownBody
+            id
+          }
+        }
+      }
+    }
+  `).allMarkdownRemark.edges;
+
+  const normalizedData = searchDataQuery.map(({ node }) => ({
+    id: node.id,
+    title: node.frontmatter?.title!,
+    content: node.rawMarkdownBody!,
+    url: node.fields?.slug!,
+  }));
+
+  return normalizedData;
+};
+
+const useLoadIndexesImpl = async () => {
+  const searchData = await useFetchSearchData();
+
+  const pageIndex: PageIndex = new FlexSearch.Document({
+    cache: 100,
+    tokenize: 'full',
+    document: {
+      id: 'id',
+      index: 'content',
+      field: ['title'],
+    },
+    context: {
+      resolution: 9,
+      depth: 2,
+      bidirectional: true,
+    },
+  });
+
+  const sectionIndex: SectionIndex = new FlexSearch.Document({
+    cache: 100,
+    tokenize: 'full',
+    document: {
+      id: 'id',
+      index: 'content',
+      tag: 'url',
+      field: ['url', 'title', 'content'],
+    },
+    context: {
+      resolution: 9,
+      depth: 2,
+      bidirectional: true,
+    },
+  });
+
+  console.log(
+    '🚀 ~ file: flex-search.tsx:75 ~ useLoadIndexesImpl ~ searchData:',
+    // JSON.stringify(searchData[0].content),
+  );
+};
+
+const Flexsearch = () => {
+  useLoadIndexesImpl();
+
+  return <div>Flexsearch</div>;
+};
+
+export default Flexsearch;
---
+++ src/apps/experimental/search/highlight-matches.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HighlightMatches = () => {
+  return <div>HighlightMatches</div>;
+};
+
+export default HighlightMatches;
