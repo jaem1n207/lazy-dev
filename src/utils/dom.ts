8eeeeb27f7473a901dfdacf02a1a6ce092c155d5
이재민 <jaem1n207@naver.com>
2023-11-25 04:38:20 +0900

    :memo: Dom 유틸 함수에 주석 추가

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -4,6 +4,9 @@
  * modified by @jaem1n207
  */
 
+/**
+ * 주어진 셀렉터에 일치하는 모든 요소를 반환합니다.
+ */
 export const getElements = (selectors: string) => {
   const elements = document.querySelectorAll(selectors);
   if (!elements.length)
@@ -12,6 +15,9 @@ export const getElements = (selectors: string) => {
   return elements;
 };
 
+/**
+ * 주어진 셀렉터에 일치하는 첫 번째 요소를 반환합니다.
+ */
 export const getElement = (selector: string) => {
   const element = document.querySelector(selector);
   if (!element) throw new Error(`There is no element that matches the selector: ${selector}`);
@@ -19,10 +25,19 @@ export const getElement = (selector: string) => {
   return element;
 };
 
+/**
+ * 주어진 요소에 클래스를 추가합니다.
+ */
 export const addClass = (element: Element, className: string) => element.classList.add(className);
 
+/**
+ * 주어진 요소에서 클래스를 제거합니다.
+ */
 export const removeClass = (element: Element, className: string) =>
   element.classList.remove(className);
 
+/**
+ * 주어진 요소가 특정 클래스를 가지고 있는지 확인합니다.
+ */
 export const hasClass = (element: Element, className: string) =>
   element.classList.contains(className);
