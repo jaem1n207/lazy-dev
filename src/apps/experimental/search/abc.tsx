8408bdda132cf9cd645eb3be94e3611a832f293a
jaem1n207 <roy.jm.lee@gmail.com>
2023-11-27 09:33:54 +0900

    :truck: 파일명 수정

--- src/apps/experimental/search/abc.tsx
+++ src/apps/experimental/search/abc.tsx
@@ -1,6 +1,6 @@
 import { RefObject, useRef, useState } from 'react';
 
-import HotKeyListener from 'Apps/common/hot-key-listener/hot-key-listener';
+import HotkeyListener from 'Apps/common/hotkey-listener/hotkey-listener';
 
 const Abc = () => {
   const [count, setCount] = useState(0);
@@ -22,14 +22,14 @@ const Abc = () => {
 
   return (
     <div>
-      <HotKeyListener
+      <HotkeyListener
         hotkey={'Enter'}
         handler={() => {
           console.log('global event handler called');
         }}
       />
       <div ref={divRef} tabIndex={-1} className="h-40pxr w-full bg-gray-400">
-        <HotKeyListener
+        <HotkeyListener
           targetRef={divRef}
           hotkey={['Control', 'Shift', 'a']}
           handler={handleIncrement}
@@ -37,14 +37,14 @@ const Abc = () => {
         Hello World: {count}
       </div>
       <input ref={inputRef1} type="text" />
-      <HotKeyListener
+      <HotkeyListener
         targetRef={inputRef1}
         hotkey={['Control', 'Shift', 'a']}
         handler={() => clearInput(inputRef1)}
       />
 
       <input ref={inputRef2} type="text" />
-      <HotKeyListener
+      <HotkeyListener
         targetRef={inputRef2}
         hotkey={['a', 'k']}
         handler={() => clearInput(inputRef2)}
