b1e811f3c3c05a75e1cb890bb6e39e8e093590ec
jaem1n207 <roy.jm.lee@gmail.com>
2023-11-28 09:21:10 +0900

    :wrench: Browser api 레거시 코드 제거

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -7,20 +7,6 @@ import type { GatsbyBrowser } from 'gatsby';
 
 export { wrapPageElement } from './gatsby-shared';
 
-// export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
-//   const isTouchDevice =
-//     'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.maxTouchPoints > 0;
-
-//   const hasMouseCapabilities = 'MouseEvent' in window;
-
-//   // 터치스크린 노트북과 같이 터치 입력과 비터치 입력을 모두 지원하는 장치에 대한 처리
-//   if (isTouchDevice || !hasMouseCapabilities) {
-//     document.documentElement.classList.remove('hide-cursor');
-//   } else {
-//     document.documentElement.classList.add('hide-cursor');
-//   }
-// };
-
 export const onServiceWorkerUpdateReady: GatsbyBrowser['onServiceWorkerUpdateReady'] = () => {
   const answer = window.confirm(
     `새로운 버전이 있어요. ` + `다시 로드하여 새로워진 블로그를 만나보세요`,
