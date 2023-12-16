04fe32eab6995d20f1ba86a55d821378eb0f88fa
이재민 <jaem1n207@naver.com>
2023-12-16 21:28:19 +0900

    :bug: 404 페이지 github issue 등록 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,17 @@
+import { window } from 'browser-monads-ts';
 import { motion } from 'framer-motion';
-import { graphql, HeadFC, navigate, PageProps } from 'gatsby';
+import { HeadFC, navigate } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import Anchor from '@/common/components/a/anchor';
 import { LoginIcon } from '@/common/components/icon/login-icon';
 import Seo from '@/common/components/seo/seo';
+import ClientOnly from '@/common/components/wrapper/client-only';
 import { ROUTES } from '@/common/const';
 import { getGithubIssueUrl } from '@/common/utils/git';
 import { animateVariant, textVariant } from '@/common/utils/motion';
 
-const NotFoundPage = (props: PageProps<Queries.NotFoundQuery>) => {
+const NotFoundPage = () => {
   const goHome = () => {
     navigate(ROUTES.HOME);
   };
@@ -54,18 +56,18 @@ const NotFoundPage = (props: PageProps<Queries.NotFoundQuery>) => {
           <LoginIcon className="mr-8pxr h-20pxr w-20pxr" />
           블로그 홈으로 돌아갈게요.
         </button>
-        <Anchor
-          className="focus-primary rounded-md bg-primary px-16pxr py-8pxr text-16pxr font-medium text-text-secondary"
-          href={getGithubIssueUrl({
-            title: `깨져있던 \`${encodeURIComponent(
-              props.location.pathname,
-            )}\` 경로를 발견했어요. 확인해주세요!`,
-            labels: 'bug',
-          })}
-          external
-        >
-          해당 문제를 여기에 알려주세요!
-        </Anchor>
+        <ClientOnly>
+          <Anchor
+            className="focus-primary rounded-md bg-bg-inner px-16pxr py-8pxr text-16pxr font-medium text-text-secondary"
+            href={getGithubIssueUrl({
+              title: `깨져있던 \`${window.location.pathname}\` 경로를 발견했어요. 확인해주세요!`,
+              labels: 'bug',
+            })}
+            external
+          >
+            해당 문제를 여기에 알려주세요!
+          </Anchor>
+        </ClientOnly>
       </motion.div>
     </div>
   );
@@ -74,13 +76,3 @@ const NotFoundPage = (props: PageProps<Queries.NotFoundQuery>) => {
 export default NotFoundPage;
 
 export const Head: HeadFC = () => <Seo title="404: Not Found" />;
-
-export const pageQuery = graphql`
-  query NotFound {
-    site(siteMetadata: {}) {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
