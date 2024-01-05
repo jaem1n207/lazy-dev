dc9e58a48878ad05f30d5a59735018b57961f301
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-05 15:18:38 +0900

    :memo: 브라우저 런타임 처음 시작 시 콘솔에 정보 출력
    콘솔 보는 맛도 있게끔

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -16,3 +16,32 @@ export const onServiceWorkerUpdateReady: GatsbyBrowser['onServiceWorkerUpdateRea
     window.location.reload();
   }
 };
+
+export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
+  const showConsole = () => {
+    const logStyles = {
+      info: [
+        'background: #86bff2',
+        'background: -webkit-linear-gradient(to right, #f64f59, #c471ed, #12c2e9)',
+        'background: linear-gradient(to right, #f64f59, #c471ed, #12c2e9)',
+        '-webkit-background-clip: text',
+        '-webkit-text-fill-color: transparent',
+        'font-size: 1.75rem',
+        'font-weight: bold',
+      ].join(';'),
+      about: ['font-size: 1.25rem', 'font-weight: bold', 'color: #86bff2'].join(';'),
+    };
+    console.log(`
+     _                    ____             
+    | |    __ _ _____   _|  _ \\  _____   __
+    | |   /  \` |_  / | | | | | |/ _ \\ \\ / /
+    | |__| (_| |/ /| |_| | |_| |  __/\\ V / 
+    |_____\\__,_/___|\\__, |____/ \\___| \\_/  
+                    |___/
+    `);
+    console.log('%cHello! I enjoy expressing what I want in code', logStyles.info);
+    console.log('%cAbout me: https://github.com/jaem1n207', logStyles.about);
+  };
+
+  showConsole();
+};
