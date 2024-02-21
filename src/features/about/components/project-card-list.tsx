a33b8685f02be9ce8e55a269fd78f092531fca4e
jaem1n207 <roy.jm.lee@gmail.com>
2024-02-21 11:57:18 +0900

    :recycle: Update project descriptions and tags

--- src/features/about/components/project-card-list.tsx
+++ src/features/about/components/project-card-list.tsx
@@ -33,20 +33,47 @@ const ProjectCardList = () => {
 
 const IMAGE_FOLDER_PATH = "../../../../content/blog/thumbnails";
 
+const stacks = {
+  svelte: {
+    name: "Svelte",
+    colorClass: "!text-red-400",
+  },
+  javascript: {
+    name: "JavaScript",
+    colorClass: "!text-yellow-400",
+  },
+  typescript: {
+    name: "TypeScript",
+    colorClass: "!text-blue-400",
+  },
+  nextjs: {
+    name: "Next.js",
+    colorClass: "!text-text-primary",
+  },
+  prisma: {
+    name: "Prisma",
+    colorClass: "!text-purple-400",
+  },
+  socketio: {
+    name: "Socket.io",
+    colorClass: "!text-yellow-400",
+  },
+  gatsby: {
+    name: "Gatsby",
+    colorClass: "!text-violet",
+  },
+  react: {
+    name: "React",
+    colorClass: "!text-green-400",
+  },
+};
+
 const projects: Omit<ComponentProps<typeof ProjectCard>, "index">[] = [
   {
     name: "Synchronize Tab Scrolling",
-    description: "여러 탭의 스크롤 위치를 동기화할 수 있는 크롬 확장 프로그램",
-    tags: [
-      {
-        name: "React",
-        colorClass: "!text-green-400",
-      },
-      {
-        name: "TypeScript",
-        colorClass: "!text-blue-400",
-      },
-    ],
+    description:
+      "여러 탭의 스크롤 위치를 동기화하는 확장 프로그램입니다. Chrome, Edge, Firefox, Opera, Brave 브라우저를 지원합니다.",
+    tags: [stacks.svelte, stacks.typescript],
     staticImageEl: (
       <StaticImage
         alt="project-thumbnail"
@@ -57,31 +84,31 @@ const projects: Omit<ComponentProps<typeof ProjectCard>, "index">[] = [
     ),
     projectUrl: {
       github: "https://github.com/jaem1n207/synchronize-tab-scrolling/tree/main",
-      live: "https://chrome.google.com/webstore/detail/synchronize-tab-scrolling/phceoocamipnafpgnchbfhkdlbleeafc/",
+      live: "https://chromewebstore.google.com/detail/synchronize-tab-scrolling/phceoocamipnafpgnchbfhkdlbleeafc?hl=en",
+    },
+  },
+  {
+    name: "eslint-plugin-ben",
+    description: "개인적인 개발 환경에 맞는 규칙을 정의한 ESLint 플러그인입니다.",
+    tags: [stacks.javascript],
+    staticImageEl: (
+      <StaticImage
+        alt="project-thumbnail"
+        src={`${IMAGE_FOLDER_PATH}/react-profile.jpg`}
+        className="h-250pxr w-full"
+        placeholder="blurred"
+      />
+    ),
+    projectUrl: {
+      github: "https://github.com/jaem1n207/eslint-plugin-ben",
+      npm: "https://www.npmjs.com/package/eslint-plugin-ben",
     },
   },
   {
     name: "Party Cinema",
     description:
       "온라인에서 친구들과 함께 동영상을 시청하고, 채팅하고 즐겁게 놀 수 있는 공간입니다.",
-    tags: [
-      {
-        name: "Next.js",
-        colorClass: "!text-text-primary",
-      },
-      {
-        name: "TypeScript",
-        colorClass: "!text-blue-400",
-      },
-      {
-        name: "Prisma",
-        colorClass: "!text-purple-400",
-      },
-      {
-        name: "Socket.io",
-        colorClass: "!text-yellow-400",
-      },
-    ],
+    tags: [stacks.nextjs, stacks.typescript, stacks.prisma, stacks.socketio],
     staticImageEl: (
       <StaticImage
         alt="project-thumbnail"
@@ -96,17 +123,8 @@ const projects: Omit<ComponentProps<typeof ProjectCard>, "index">[] = [
   },
   {
     name: "Tech Blog",
-    description: "프론트엔드 웹 개발자를 위한 기술 블로그",
-    tags: [
-      {
-        name: "Gatsby",
-        colorClass: "!text-violet",
-      },
-      {
-        name: "TypeScript",
-        colorClass: "!text-blue-400",
-      },
-    ],
+    description: "웹 개발을 하며 얻은 점을 다루는 기술 블로그입니다.",
+    tags: [stacks.gatsby, stacks.typescript],
     staticImageEl: (
       <StaticImage
         alt="project-thumbnail"
@@ -121,17 +139,8 @@ const projects: Omit<ComponentProps<typeof ProjectCard>, "index">[] = [
   },
   {
     name: "JM Wordle",
-    description: "Josh Wardle의 인기 게임인 Wordle에 약간의 기능을 추가한 웹 사이트",
-    tags: [
-      {
-        name: "React",
-        colorClass: "!text-green-400",
-      },
-      {
-        name: "TypeScript",
-        colorClass: "!text-blue-400",
-      },
-    ],
+    description: "Josh Wardle의 인기 게임인 Wordle에 약간의 기능을 추가한 Wordle 입니다.",
+    tags: [stacks.react, stacks.typescript],
     staticImageEl: (
       <StaticImage
         alt="project-thumbnail"
